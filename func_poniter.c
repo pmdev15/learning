d10f7bcc4d1256b56732bef6d0a5002c3cc3128e
pmdev15 <pmdev1566@gmail.com>
2025-06-27 00:49:13 +0530

    27-06-2025

--- func_poniter.c
+++ func_poniter.c
@@ -3,7 +3,9 @@
 int add(int a,int b){
     return a+b;
 }
-
+int sub(int a,int b){
+	return a-b;
+}
 int main(){
 
 
@@ -15,8 +17,16 @@ int main(){
                       // i.e no need to use * just result = ptr(a,b);
 
     // using func pointer
-    int result = *ptr(a,b);
+    int result = ptr(a,b);
     printf("%d\n",result);
+
+	int (*ptr2func[2])(int,int) = {add,sub};
+	int choice;
+	printf("enter 0 for addition and 1 for subs\n");
+	scanf("%d\n",&choice);
+	printf("enter two number\n");
+	scanf("%d %d\n",&a,&b);
+	printf("%d\n",ptr2func[choice](a,b));
 
     return 0;
 }
